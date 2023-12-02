42a183766f2afbd87d9d64e4161fd08a3832ed3d
Vinicius Gaspar <vinicius.gaspar@csptech.com.br>
2023-12-02 20:07:39 -0300

    add create table

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -33,6 +33,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
 	defer db.Close()
 
 	rabbitMQChannel := getRabbitMQChannel()
